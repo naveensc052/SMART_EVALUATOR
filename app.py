fc9b4a00d1bb08cb27a4103319585f79fd4a2481
Naveen <106677907+naveensc052@users.noreply.github.com>
2024-03-15 01:23:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,7 +8,6 @@ from langchain.vectorstores import FAISS
 from langchain.llms import OpenAI
 from langchain.chains.question_answering import load_qa_chain
 from langchain.callbacks import get_openai_callback
-from nltk.tokenize import sent_tokenize
 from sentence_transformers import SentenceTransformer,util
 import openai
 
